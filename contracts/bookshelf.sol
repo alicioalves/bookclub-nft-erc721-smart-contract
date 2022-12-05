764cc10cfd92956938c1639443810be1d10e18c8
Alicio <alicio.alvesm@gmail.com>
2022-12-04 23:29:01 -0300

    feat: ERC721 functions

--- contracts/bookshelf.sol
+++ contracts/bookshelf.sol
@@ -15,6 +15,7 @@ contract Bookshelf is ERC721 {
     Book[] bookList;
     mapping(uint256 => address) bookToOwner;
     mapping(address => uint256) ownerBookCount;
+    mapping(uint256 => address) bookApprovals;
 
     function addBook(
         string memory _name,
@@ -24,5 +25,40 @@ contract Bookshelf is ERC721 {
         uint256 id = bookList.push(Book(_name, _year, _author));
         bookToOwner[id] = msg.sender;
         ownerBookCount[msg.sender] = ownerBookCount[msg.sender].add(1);
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerBookCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return bookToOwner[_tokenId];
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {
+        require(
+            bookToOwner[_tokenId] == msg.sender ||
+                bookApprovals[_tokenId] == msg.sender
+        );
+
+        ownerBookCount[_to] = ownerBookCount[_to].add(1);
+        ownerBookCount[msg.sender] = ownerBookCount[msg.sender].sub(1);
+        bookToOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId)
+        external
+        payable
+        onlyOwnerOf(_tokenId)
+    {
+        require(bookToOwner[_tokenId] == msg.sender);
+        bookApprovals[_tokenId] == _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
     }
 }
