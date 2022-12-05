ded1f2465bc0b12e105624a1b2a12b0d2b8662a0
Alicio <alicio.alvesm@gmail.com>
2022-12-04 23:17:00 -0300

    feat: first steps of Bookshelf smart contract

---
+++ contracts/bookshelf.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./safemath.sol";
+
+contract Bookshelf {
+    using SafeMath for uint256;
+
+    struct Book {
+        string name;
+        uint256 year;
+        string author;
+    }
+
+    Book[] bookList;
+    mapping(uint256 => address) bookToOwner;
+    mapping(address => uint256) ownerBookCount;
+
+    function addBook(
+        string memory _name,
+        uint256 _year,
+        string memory _author
+    ) public {
+        uint256 id = bookList.push(Book(_name, _year, _author));
+        bookToOwner[id] = msg.sender;
+        ownerBookCount[msg.sender] = ownerBookCount[msg.sender].add(1);
+    }
+}
